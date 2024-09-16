dcb52fe0a044d31c5811e595abe7bf4e65e16f42
Mark Hakansson <mark.hakansson@live.se>
2024-09-16 21:16:09 +0200

    Add entropy calculation

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,24 @@ use std::{
 
 const DEFAULT_WORDLIST: &str = "/usr/share/dict/words";
 
+/// A simple tool for generating passphrases.
+///
+/// By default, uses the system default wordlist if no wordlist is specified.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Number of words to use.
-    #[arg(short, long)]
+    /// How many number of words the passphrase should consist of.
+    #[arg(short = 'c', long)]
     count: usize,
-    /// Optional separator to use between words.
-    #[arg(long)]
+    /// Optional string separator to use between words.
+    #[arg(short = 's', long)]
     word_separator: Option<String>,
     /// Optional wordlist to use.
     #[arg(short, long)]
     wordlist: Option<PathBuf>,
+    /// Additional information about the generated passphrase.
+    #[arg(short, long, default_value = "false")]
+    verbose: bool,
 }
 
 struct PwOptions {
@@ -78,6 +84,10 @@ fn get_pw_options(args: &Args) -> PwOptions {
     }
 }
 
+fn calculate_entropy(num_words: usize, pool_size: usize) -> f64 {
+    return (pool_size as f64).powi(num_words as i32).log2();
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -90,5 +100,11 @@ fn main() {
     let wordlist = read_worldlist(wordlist_path);
     let pass = generate_passphrase(&wordlist, &options);
 
-    println!("{pass}");
+    if args.verbose {
+        let entropy = calculate_entropy(options.count, wordlist.len());
+        println!("@ entropy: {entropy:.2} bits");
+        println!("pass: {pass}");
+    } else {
+        println!("{pass}");
+    }
 }
